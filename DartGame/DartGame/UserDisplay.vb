d960392981605ea8a2b2381e7d77f601decdc2a7
Masaharu41 <dalekrebel@gmail.com>
2024-09-10 04:26:09 -0600

    Fixed read in and out of bounds error

--- DartGame/DartGame/UserDisplay.vb
+++ DartGame/DartGame/UserDisplay.vb
@@ -10,7 +10,6 @@ Public Class UserDisplay
         Dim players As String
         Dim temp() As String
         Dim display As String
-        Dim e As Integer = 0
 
         Try
             FileOpen(1, "..\..\Replay.txt", OpenMode.Input)
@@ -24,17 +23,21 @@ Public Class UserDisplay
         Do Until EOF(1)
             players = LineInput(1)
             players = players.Replace(Chr(34), " ")
+
+
             temp = Split(players, ":")
-            pastUser = Split(temp(0), " User$$")
-            scoreOne = Split(temp(1), "Play1##")
-            scoreTwo = Split(temp(2), "Play2##")
-            scoreThree = Split(temp(3), "Play3##")
-            e = e + 1
+            If temp.Length = 1 Then
+            Else
+
+                pastUser = Split(temp(0), " User$$")
+                scoreOne = Split(temp(1), "Play1##")
+                scoreTwo = Split(temp(2), "Play2##")
+                scoreThree = Split(temp(3), "Play3##")
+                display = $"{pastUser(1)} 1, {scoreOne(1)}: 2, {scoreTwo(1)}: 3, {scoreThree(1)} "
+                RecordsListBox.Items.Add(display)
+            End If
         Loop
-        For i = 1 To pastUser.Length - 2
-            display = $"{pastUser(i)} 1, {scoreOne(i)}: 2, {scoreTwo(i)}: 3, {scoreThree(i)} "
-            RecordsListBox.Items.Add(display)
-        Next
+
 
         FileClose(1)
     End Sub
