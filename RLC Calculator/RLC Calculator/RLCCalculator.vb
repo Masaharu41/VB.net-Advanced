009a167c600b54ef3807f041abd0be3e6d2111e7
Masaharu41 <dalekrebel@gmail.com>
2024-09-16 19:25:08 -0600

    began adding data to global array

--- RLC Calculator/RLC Calculator/RLCCalculator.vb
+++ RLC Calculator/RLC Calculator/RLCCalculator.vb
@@ -8,14 +8,13 @@ Option Strict On
 
 Public Class RLCCalculator
 
+    Public massArray(20) As Double
+
     Function C2Calculator() As Double
         Dim C2 As Double               ' Calculate the reactance of c2
         Dim pi As Double = Math.PI
-
-
-
         C2 = 1 / (2 * (pi) * CInt(FreqTextBox.Text) * CInt(C2TextBox.Text))
-
+        massArray(3) = C2
         Return C2
     End Function
     Function C1Calculator() As Double
@@ -23,6 +22,7 @@ Public Class RLCCalculator
         Dim pi As Double = Math.PI
 
         C1 = 1 / (2 * pi * CInt(FreqTextBox.Text) * CInt(C1TextBox.Text))
+        massArray(0) = C1
         Return C1
 
     End Function
@@ -32,6 +32,7 @@ Public Class RLCCalculator
         Dim pi As Double = Math.PI
 
         L1 = (2 * pi * CInt(FreqTextBox.Text) * CInt(L1TextBox.Text))
+        massArray(6) = L1
         Return L1
     End Function
 
@@ -41,7 +42,8 @@ Public Class RLCCalculator
 
         sendArray(0) = Math.Sqrt((l1Value ^ 2) + (CDbl(SeriesRTextBox.Text) ^ 2))
         sendArray(1) = Math.Atan(l1Value / CDbl(SeriesRTextBox.Text))
-
+        massArray(9) = sendArray(0)
+        massArray(10) = sendArray(1)
         Return sendArray
     End Function
 
@@ -51,7 +53,8 @@ Public Class RLCCalculator
 
         sendArray(0) = Math.Sqrt((c2Value ^ 2) + (CDbl(R2TextBox.Text) ^ 2))
         sendArray(1) = Math.Atan(c2Value / CDbl(R2TextBox.Text))
-
+        massArray(11) = sendArray(0)
+        massArray(12) = sendArray(1)
         Return sendArray
     End Function
 
@@ -81,11 +84,18 @@ Public Class RLCCalculator
         sendArray(1) = demoniatorAngle - temp(1)
         sendArray(2) = sendArray(0) * Math.Cos(sendArray(1))
         sendArray(3) = sendArray(0) * Math.Sin(sendArray(1))
+        massArray(13) = demoniatorRect
+        massArray(14) = demoniatorAngle
+        massArray(15) = sendArray(0)
+        massArray(16) = sendArray(1)
+        massArray(17) = sendArray(2)
+        massArray(18) = sendArray(3)
+
 
         Return sendArray
     End Function
 
-    Function TotalReactace() As Double()
+    Function TotalReactaces() As Double()
         Dim sendArray(3) As Double           ' solve for total reactance and return an array with both polar and rect.
         Dim parallel() As Double = ParallelArray()
         Dim seriesCap As Double = C1Calculator()
@@ -95,23 +105,38 @@ Public Class RLCCalculator
         sendArray(3) = -seriesCap + parallel(3)
         sendArray(0) = Math.Sqrt((sendArray(2) ^ 2) + (sendArray(3) ^ 2))
         sendArray(1) = Math.Atan(sendArray(3) / sendArray(2))
+        massArray(19) = sendArray(0)
+        massArray(20) = sendArray(1)
+        massArray(21) = sendArray(2)
+        massArray(22) = sendArray(3)
+
 
         Return sendArray
 
     End Function
 
     Function TotalCurrents() As Double
-        Dim reactance() As Double = TotalReactace()   ' solve for series current
+        Dim reactance() As Double = TotalReactaces()   ' solve for series current
         Dim voltageGen As Double = CDbl(VoltTextBox.Text)
+        Dim sender As Double
 
+        sender = voltageGen / reactance(0)
 
-
-
-    End Function
-
-    Function ValidData() As Boolean
+        Return sender
 
     End Function
+
+    Sub BigCalculator()
+        ' final sub that finishes voltage and current calcs
+        ' writes to the text box as well as the file
+        'Dim parallel() As Double = ParallelArray()
+        'Dim 
+
+    End Sub
+
+    'Function ValidData() As Boolean
+
+    'End Function
     Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
 
     End Sub
