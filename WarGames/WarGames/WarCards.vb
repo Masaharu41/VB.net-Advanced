cdce80e6db56e82b4b26b1b8daf63fcf4396d204
Owen Fujii <dalekrebel@gmail.com>
2024-09-23 21:17:47 +0100

    Added TODO

--- WarGames/WarGames/WarCards.vb
+++ WarGames/WarGames/WarCards.vb
@@ -1,3 +1,15 @@
-﻿Public Class WarCards
+﻿'Owen Fujii
+'War Card Game
+'TODO
+'{} Create a random deck for each player using standard 52 card deck 50/50
+'{} Display Cards Graphically
+'{} Track Results of each card turn
+'
+
+
+
+
+
+Public Class WarCards
 
 End Class
