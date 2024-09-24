78185fbc640064f3d5c3a2946c01c3d6ed9b4751
Masaharu41 <dalekrebel@gmail.com>
2024-09-24 09:35:11 -0600

    Began implementing playing sub

--- WarGames/WarGames/WarCards.vb
+++ WarGames/WarGames/WarCards.vb
@@ -12,6 +12,8 @@ Option Explicit On
 Public Class WarCards
     Dim player1(26, 1) As Integer
     Dim player2(26, 1) As Integer
+    Dim player1Wins() As Integer
+    Dim player2Wins() As Integer
 
     Sub CreateDecks()
         Shuffler.DrawCard(player1, player2)
@@ -21,4 +23,34 @@ Public Class WarCards
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles DealButton.Click
         CreateDecks()
     End Sub
+
+    Function PlayGame() As Integer
+        Static Dim playCount%, twoWin%, oneWin%
+        Dim player1Card%, player2Card%
+
+        If playCount <= 26 Then
+            player1Card = player1(playCount, 0)
+            player2Card = player2(playCount, 0)
+        Else
+            playCount = 0
+            twoWin = 0
+            oneWin = 0
+            Return -1
+        End If
+
+        If player1Card < player2Card Then
+            player2Wins(twoWin) = player2Card
+            twoWin += twoWin
+        ElseIf player1Card = player2Card Then
+
+        Else
+            player1Wins(playCount) = player2Card
+            oneWin += oneWin
+        End If
+
+        playCount += playCount
+        Return playCount
+    End Function
+
+
 End Class
