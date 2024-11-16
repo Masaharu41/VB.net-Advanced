02205ac0c1f487863c639448defa7bc26423d24e
Masaharu41 <dalekrebel@gmail.com>
2024-11-16 13:12:23 -0700

    Added beginning TODO

--- Home HVAC Control/Home HVAC Control/HVAC Gui.vb
+++ Home HVAC Control/Home HVAC Control/HVAC Gui.vb
@@ -5,6 +5,20 @@
 Option Strict On
 Option Explicit On
 Option Compare Binary
+
+'TODO
+'{} Create GUI with toolstrip for serial setup
+'{} Display active time on GUI 
+'{} Display Temperature with state of cooling, heat, and fan
+'{} Allow user to adjust 0.5 degree increments in a high and low setpoint boxes 
+'{} Only allow room temperature to be applied from 50 to 90 degrees farenheit
+'{} Analog Input 1 as the overall temperature of from
+'{} Analog Input 2 as the temperature of the heating and cooling system
+'{} Poll heating and cooling temperature every 2 min
+'{} Thermal protection that shutsdown unit if temperature does not perform as desired
+'{} Digital Input 1 handles safety interlock // low = error // Display error on GUI
+'{} Display safety interlock error on Digital Output 1
+'{} Digital Input 2 controls heating function
 Public Class HVACGuiForm
 
     Public GrowlGreyLight As Color = Color.FromArgb(230, 213, 232)
